11da7d182d626eebabc10d22639738f79781c482
dalaw <danlawless1999@gmail.com>
2026-06-07 04:34:03 +1000

    pushing

---
+++ backend/migrations/versions/2026_05_25_0836-d383e29e29c4_merge_branches.py
@@ -0,0 +1,25 @@
+"""merge branches
+
+Revision ID: d383e29e29c4
+Revises: 9b3d4f7a8c21, d15dee848b33
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd383e29e29c4'
+down_revision: Union[str, None] = ('9b3d4f7a8c21', 'd15dee848b33')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
